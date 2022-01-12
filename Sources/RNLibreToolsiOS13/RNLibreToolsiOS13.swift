588c3c69c77d7e7213bf9c2920db9418954dd48c
Lasha Maruashvili <38511844+maruashvililasha@users.noreply.github.com>
2022-01-12 20:13:01 +0400

    Raw Trend

--- Sources/RNLibreToolsiOS13/RNLibreToolsiOS13.swift
+++ Sources/RNLibreToolsiOS13/RNLibreToolsiOS13.swift
@@ -73,12 +73,7 @@ public class RNLibreToolsiOS13 : RnLibreToolsProtocol {
         var trend : [Double] = history.factoryTrend.map({Double($0.value)})
         //.map({((Double($0.value) / 18.0182) * 10).rounded() / 10})
         let current = trend.remove(at: 0)
-        let rawHistory: [Double] = history.factoryValues.map({Double($0.value)})//.map({((Double($0.value) / 18.0182) * 10).rounded() / 10})
-        print("\n###############")
-        print("Factory Values :", history.factoryValues, separator: " - ")
-        print("Raw Trend :", history.rawTrend, separator: " - ")
-        print("raw Values :", history.rawValues, separator: " - ")
-        print("###############\n")
+        let rawHistory: [Double] = history.rawTrend.map({Double($0.value)})//.map({((Double($0.value) / 18.0182) * 10).rounded() / 10})
         let response = [[
             "currentGluecose" : [current],
             "trendHistory" : trend,
