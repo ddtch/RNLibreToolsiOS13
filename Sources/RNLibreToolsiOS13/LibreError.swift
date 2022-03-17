6102745640a806a1be3aee17c9347c9bb8598b95
dd <4daveid@gmail.com>
2022-03-17 13:21:22 +0400

    changed readme

--- Sources/RNLibreToolsiOS13/LibreError.swift
+++ Sources/RNLibreToolsiOS13/LibreError.swift
@@ -1,8 +1,9 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Lasha Maruashvili on 13.12.21.
+//  Edited by D_d
 //
 
 import Foundation
@@ -10,7 +11,7 @@ import Foundation
 public struct LibreError : Error {
     public let errorCode: Int
     public let errorMessage: String
-    
+
     init(errorCode: Int, errorMessage: String) {
         self.errorCode = errorCode
         self.errorMessage = errorMessage
